a9bb46c3ab806d8af7e3d1fafc7e4a50c08fe4f1
Fandhika Azhar <99522867+fandhikazhr@users.noreply.github.com>
2022-07-13 01:59:53 +0700

    Update iptvINDO.sh

--- iptvINDO.sh
+++ iptvINDO.sh
@@ -16,3 +16,29 @@ echo "4. Kompas TV"
 echo "5. Trans7"
 echo "6. Trans TV"
 echo "7. TVOne"
+echo "Pilih Channel : "
+read pilih
+
+case "$pilih" in 
+  "1")
+    mpv http://free.fullspeed.tv/iptv-query?streaming-ip=https://youtube.com/user/NetInsights/live
+    ;;
+  "2")
+    mpv http://free.fullspeed.tv/iptv-query?streaming-ip=https://m.youtube.com/c/MetrotvnewsOfficial/live
+    ;;
+  "3")
+    mpv https://live.cnnindonesia.com/livecnn/smil:cnntv.smil/playlist.m3u8
+    ;;
+  "4")
+    mpv https://live-kg.jixie.media/live/kompastv_lhd.m3u8
+    ;;
+  "5")
+    mpv https://video.detik.com/trans7/smil:trans7.smil/playlist.m3u8
+    ;;
+  "6")
+    mpv https://video.detik.com/transtv/smil:transtv.smil/playlist.m3u8
+    ;;
+  "7")
+    mpv http://free.fullspeed.tv/iptv-query?streaming-ip=https://youtube.com/watch?v=K9lOPKwo44w
+    ;;
+esac
